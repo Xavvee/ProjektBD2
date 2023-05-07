d3b11015adb60408e838e3d2f048539270f17a53
S0jer <ppjaskowiec@gmail.com>
2023-05-07 16:47:32 +0200

    Add basic login page - might change look later

--- frontend/frontend/src/app/login/login.component.ts
+++ frontend/frontend/src/app/login/login.component.ts
@@ -1,15 +1,25 @@
+import { AuthService } from '../auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
+  str = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  sendItem() {
+    this.authService.SignIn(this.email, this.password);
   }
 
+  newUser() {
+    this.email = '';
+    this.password = '';
+  }
 }
