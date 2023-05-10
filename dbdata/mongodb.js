17a2030514e22b7a8a6a5e72ecbd3016df6baf23
Rafał Laskowski <rafalek_0202@wp.pl>
2023-05-10 20:09:10 +0200

    new scheme for database

--- dbdata/mongodb.js
+++ dbdata/mongodb.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://default:default@localhost:27017/db_project";
+const uri = "mongodb://admin:admin@localhost:27017";
 
 console.log("Attempting to connect to MongoDB");
 
@@ -12,6 +12,111 @@ console.log("Attempting to connect to MongoDB");
 
         const db = client.db("db_project");
         console.log("Creating collections...");
+
+        // Clients collection
+        await db.createCollection("Clients", {
+            validator: {
+                $jsonSchema: {
+                    bsonType: "object",
+                    required: [
+                        "userId",
+                        "firstName",
+                        "lastName",
+                        "datOfBirth",
+                        "email",
+                        "phone",
+                        "address",
+                        "registerDate",
+                    ],
+                    properties: {
+                        userId: { bsonType: "int" },
+                        firstName: { bsonType: "string" },
+                        lastName: { bsonType: "string" },
+                        datOfBirth: { bsonType: "date" },
+                        email: { bsonType: "string" },
+                        phone: { bsonType: "string" },
+                        address: { bsonType: "string" },
+                        registerDate: { bsonType: "date" },
+                        reservations: {
+                            bsonType: "array",
+                            items: {
+                                bsonType: "object",
+                                required: [
+                                    "reservationId",
+                                    "reservationStatus",
+                                    "peopleCount",
+                                    "startDate",
+                                    "endDate",
+                                    "games",
+                                    "tables"
+                                ],
+                                properties: {
+                                    reservationId: { bsonType: "int" },
+                                    reservationStatus: { bsonType: "string" },
+                                    peopleCount: { bsonType: "int" },
+                                    startDate: { bsonType: "date" },
+                                    endDate: { bsonType: "date" },
+                                    games: {
+                                        bsonType: "array",
+                                        items: {
+                                            bsonType: "object",
+                                            required: [
+                                                "gameId",
+                                                "gameType",
+                                                "capacity",
+                                                "pricePerHour",
+                                                "tables"
+                                            ],
+                                            properties: {
+                                                gameId: { bsonType: "int" },
+                                                gameType: { bsonType: "string" },
+                                                capacity: { bsonType: "int" },
+                                                pricePerHour: { bsonType: "string" },
+                                                tables: {
+                                                    bsonType: "array",
+                                                    items: {
+                                                        bsonType: "object",
+                                                        required: [
+                                                            "tableId",
+                                                            "capacity"
+                                                        ],
+                                                        properties: {
+                                                            tableId: { bsonType: "int" },
+                                                            capacity: { bsonType: "int" },
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    },
+                                    orders: {
+                                        bsonType: "array",
+                                        items: {
+                                            bsonType: "object",
+                                            required: [
+                                                "orderId",
+                                                "dishes",
+                                                "orderDate",
+                                                "finalPrice"
+                                            ],
+                                            properties: {
+                                                orderId: { bsonType: "int" },
+                                                dishes: {
+                                                    bsonType: "array",
+                                                    items: { bsonType: "int" }
+                                                },
+                                                orderDate: { bsonType: "date" },
+                                                finalPrice: { bsonType: "string" },
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
 
         // Employees collection
         await db.createCollection("Employees", {
@@ -42,34 +147,7 @@ console.log("Attempting to connect to MongoDB");
             },
         });
 
-        // Clients collection
-        await db.createCollection("Clients", {
-            validator: {
-                $jsonSchema: {
-                    bsonType: "object",
-                    required: [
-                        "userId",
-                        "firstName",
-                        "lastName",
-                        "datOfBirth",
-                        "email",
-                        "phone",
-                        "address",
-                        "registerDate",
-                    ],
-                    properties: {
-                        userId: {bsonType: "int"},
-                        firstName: {bsonType: "string"},
-                        lastName: {bsonType: "string"},
-                        datOfBirth: {bsonType: "date"},
-                        email: {bsonType: "string"},
-                        phone: {bsonType: "string"},
-                        address: {bsonType: "string"},
-                        registerDate: {bsonType: "date"},
-                    },
-                },
-            },
-        });
+
 
         // Games collection
         await db.createCollection("Games", {
@@ -82,68 +160,25 @@ console.log("Attempting to connect to MongoDB");
                         gameType: {bsonType: "string"},
                         capacity: {bsonType: "int"},
                         pricePerHour: {bsonType: "string"},
+                        tables: {
+                            bsonType: "array",
+                            items: {
+                                bsonType: "object",
+                                required: [
+                                    "tableId",
+                                    "capacity"
+                                ],
+                                properties: {
+                                    tableId: { bsonType: "int" },
+                                    capacity: { bsonType: "int" },
+                                }
+                            }
+                        }
                     },
                 },
             },
         });
 
-        // Tables collection
-        await db.createCollection("Tables", {
-            validator: {
-                $jsonSchema: {
-                    bsonType: "object",
-                    required: ["tableId", "capacity"],
-                    properties: {
-                        tableId: {bsonType: "int"},
-                        capacity: {bsonType: "int"},
-                    },
-                },
-            },
-        });
-
-        // Reservations collection
-        await db.createCollection("Reservations", {
-            validator: {
-                $jsonSchema: {
-                    bsonType: "object",
-                    required: [
-                        "reservationId",
-                        "reservationStatus",
-                        "tableId",
-                        "gameId",
-                        "peopleCount",
-                        "startDate",
-                        "endDate",
-                    ],
-                    properties: {
-                        reservationId: {bsonType: "int"},
-                        reservationStatus: {bsonType: "string"},
-                        tableId: {bsonType: "int"},
-                        gameId: {bsonType: "int"},
-                        peopleCount: {bsonType: "int"},
-                        startDate: {bsonType: "date"},
-                        endDate: {bsonType: "date"},
-                    },
-                },
-            },
-        });
-
-        // Orders collection
-        await db.createCollection("Orders", {
-            validator: {
-                $jsonSchema: {
-                    bsonType: "object",
-                    required: ["orderId", "reservationId", "dishes", "orderDate", "finalPrice"],
-                    properties: {
-                        orderId: {bsonType: "int"},
-                        reservationId: {bsonType: "int"},
-                        dishes: {bsonType: "array", items: {bsonType: "int"}},
-                        orderDate: {bsonType: "date"},
-                        finalPrice: {bsonType: "string"},
-                    },
-                },
-            },
-        });
 
         // Menu collection
         await db.createCollection("Menu", {
